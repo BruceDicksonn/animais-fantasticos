866fa6ebdf91da4b9eca759756560ad9ced6e93b
BruceDicksonn <dicksonbruce1110@gmail.com>
2023-05-11 17:44:54 -0300

    accordion transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import ScrollSuave from "./modules/scroll-suave.js"; 
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
@@ -12,10 +12,13 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+const accordion = new Accordion('[data-anime="accordion"] dt');
+
 scrollSuave.init();
+accordion.init();
+
 
 initAnimacaoScroll();
-initAccordion();
 initTabNav();
 initModal();
 initTooltip();
