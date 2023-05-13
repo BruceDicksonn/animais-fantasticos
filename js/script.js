ae2afe4b17641a85619028a8da22b5b43ba27e7b
BruceDicksonn <dicksonbruce1110@gmail.com>
2023-05-13 13:00:00 -0300

    modal refatorado com class

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimacaoScroll from "./modules/scroll-animacao.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tab-nav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -14,14 +14,15 @@ import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 const accordion = new Accordion('[data-anime="accordion"] dt');
 const tabnav = new TabNav('[data-tab-menu="tabmenu"] li','[data-tab="content"] section');
+const modal = new Modal('[data-modal="abrir"]','[data-modal="fechar"]','[data-modal="container"]');
 
 scrollSuave.init();
 accordion.init();
 tabnav.init();
+modal.init();
 
 
 initAnimacaoScroll();
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
