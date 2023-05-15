2dec00482d2f48072f675d2982145012fd2c8f27
BruceDicksonn <dicksonbruce1110@gmail.com>
2023-05-15 18:29:24 -0300

    adicionado slide de fotos

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ import MenuMobile from "./modules/menu-mobile.js";
 import Funcionamento from "./modules/funcionamento.js";
 import fetchAnimais from "./modules/fetch-animais.js";
 import fetchBitcoin from "./modules/fetch-bitcoin.js";
+import SlideNav from './modules/slide.js'
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 const accordion = new Accordion('[data-anime="accordion"] dt');
@@ -21,6 +22,7 @@ const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+const slide = new SlideNav('.slide', '.slide-wrapper');
 
 scrollSuave.init();
 accordion.init();
@@ -31,6 +33,9 @@ scrollAnima.init();
 dropdownMenu.init();
 menuMobile.init();
 funcionamento.init();
+
+slide.init();
+slide.addControl('.custom-controls');
 
 fetchAnimais('./animaisapi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker','.btc-preco');
